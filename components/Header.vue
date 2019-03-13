a14732a2db1cd0504aa4207422b1e3f0c20616d7
DacD <dacdao01@gmail.com>
2019-03-13 02:52:52 -0400

    Header components serve as nav

---
+++ components/Header.vue
@@ -0,0 +1,43 @@
+<template>
+  <header>
+    <nav>
+      <ul>
+        <li>
+          <nuxt-link to="/">Starting page</nuxt-link>
+        </li>
+        <li>
+          <nuxt-link to="/recipes">Recipes</nuxt-link>
+        </li>
+        <li>
+          <nuxt-link to="/about">About</nuxt-link>
+        </li>
+      </ul>
+    </nav>
+  </header>
+</template>
+
+<style scoped>
+header {
+  width: 100%;
+  height: 30px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  background-color: #f0ffff;
+}
+ul {
+  list-style: none;
+  padding: 0;
+  margin: 0;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+li {
+  margin: 0 10px;
+}
+a {
+  text-decoration: none;
+  color: black;
+}
+</style>
